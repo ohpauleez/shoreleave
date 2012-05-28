a70bef778185e99f0552e688aa399e50ecc48b9f
Paul deGrandis <paul.degrandis@gmail.com>
2012-05-27 21:52:26 -0400

    Fixed up some typos

--- project.clj
+++ project.clj
@@ -1,5 +1,10 @@
 (defproject shoreleave "0.1.0-SNAPSHOT"
   :description "A smarter client-side with ClojureScript"
+  :url "http://github.com/ohpauleez/shoreleave"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "See the notice in README.mkd or details in LICENSE.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [noir "1.3.0-beta3"]]
   :dev-dependencies [[vimclojure/server "2.3.1" :exclusions [org.clojure/clojure]] 
@@ -10,7 +15,7 @@
                      ;[cdt "1.2.6.2-SNAPSHOT"]
                      ;[lein-cdt "1.0.0"] ; use lein cdt to attach
                      ]
-  :plugins  [[lein-cljsbuild "0.1.8"]]
+  :plugins  [[lein-cljsbuild "0.1.10"]]
   ;:cljsbuild {:source-path "src"
   ;            :compiler {:output-dir "resources/public/cljs/"
   ;                       :output-to "resources/public/cljs/bootstrap.js"
