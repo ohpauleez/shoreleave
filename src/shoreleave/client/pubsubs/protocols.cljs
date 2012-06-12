40f0839779b839c9a3bd20a585160ee54b4fd21a
Paul deGrandis <paul.degrandis@gmail.com>
2012-06-11 22:47:11 -0400

    reorg pubsub

--- src/shoreleave/client/pubsubs/protocols.cljs
+++ src/shoreleave/client/pubsubs/protocols.cljs
@@ -1,4 +1,5 @@
-(ns shoreleave.client.pubsubs.protocols)
+(ns shoreleave.client.pubsubs.protocols
+  (:require [shoreleave.client.efunction :as efn]))
 
 (defprotocol IMessageBrokerBus
   (subscribe [broker-bus handler-fn topic])
@@ -12,4 +13,52 @@
   (topicify [t])
   (publishized? [t])
   (publishize [t broker-bus]))
+
+
+(extend-protocol IPublishable
+
+  function
+  (topicify [t]
+    (or (publishized? t)
+        (str `t)))
+  (publishized? [t]
+    (:sl-published (meta t)))
+  (publishize [fn-as-topic bus]
+    (if (publishized? fn-as-topic)
+      fn-as-topic
+      (let [published-topic (topicify fn-as-topic)
+            new-meta (assoc (meta fn-as-topic) :sl-published published-topic)]
+        (efn/Function. (fn [& args]
+                         (let [ret (apply fn-as-topic args)]
+                           (publish bus published-topic ret)
+                           ret))
+                       new-meta))))
+
+  efn/Function
+  (topicify [t]
+    (or (publishized? t)
+        (str `t)))
+  (publishized? [t]
+    (:sl-published (meta t)))
+  (publishize [fn-as-topic bus]
+    (if (publishized? fn-as-topic)
+      fn-as-topic
+      (let [published-topic (topicify fn-as-topic)
+            new-meta (assoc (meta fn-as-topic) :sl-published published-topic)]
+        (efn/Function. (fn [& args]
+                         (let [ret (apply (.-f fn-as-topic) args)]
+                           (publish bus published-topic ret)
+                           ret))
+                       new-meta))))
+
+  default
+  (topicify [t]
+    (str t)))
+
+#_(subscribe my-listener source-fn)
+#_(subscribe my-listner "cats")
+#_(subscribe my-listener (atom {}))
+#_(defn some-fn [] 5)
+#_(def pub-some-fn (publishize some-fn))
+#_(def pub-some-watchable (publishize (atom {})))
 
