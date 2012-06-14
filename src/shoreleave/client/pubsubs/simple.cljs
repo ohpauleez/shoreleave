53a1c4f4141de48ffc6b064263c3d8b44f3f7be6
Paul deGrandis <paul.degrandis@gmail.com>
2012-06-13 20:49:28 -0400

    pubsub order of args corrected

--- src/shoreleave/client/pubsubs/simple.cljs
+++ src/shoreleave/client/pubsubs/simple.cljs
@@ -6,24 +6,33 @@
 ;; This is defined in ns barker.client.main, but ` resolves ns to user
 ;(js/console.log (keyword `process-search)
 ;(js/console.log (keyword (symbol "barker.client.main" (name 'process-search))))
+;all *ns* things are missing
 
 (extend-type goog.pubsub.PubSub
   ps-protocols/IMessageBrokerBus
-  (subscribe [bus handler-fn topic]
+  (subscribe [bus topic handler-fn]
     (.subscribe bus (ps-protocols/topicify topic) handler-fn))
 
-  (subscribe-once [bus handler-fn topic]
+  (subscribe-once [bus topic handler-fn]
     (.subscribeOnce bus (ps-protocols/topicify topic) handler-fn))
 
-  (unsubscribe [bus handler-fn topic]
+  #_(subscribe-> [bus & chain-handler-fns]
+    (let [subscripts (partition 2 1 chain-handler-fns)]
+      (when-not (empty? subscripts)
+        (doseq [[t h] subscripts]
+          (ps-protocols/subscribe bus t h)))))
+
+  (unsubscribe [bus topic handler-fn]
     (.unsubscribe bus (ps-protocols/topicify topic) handler-fn))
 
   (publish
     ([bus topic data]
      (.publish bus (ps-protocols/topicify topic) data))
     ([bus topic data & more-data]
-     (.publish bus (ps-protocols/topicify topic) (into [data] more-data)))))
+     (.publish bus (ps-protocols/topicify topic) (into [data] more-data))))
 
+  IHash
+  (-hash [bus] (goog.getUid bus)))
 
 (defn subscribers-count [bus topic]
   (.getCount bus (ps-protocols/topicify topic)))
